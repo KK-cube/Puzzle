8649fa7291922c9b35729987a1d66a58782884c3
河村孝太郎 <kawakou@kawamurakoutarounoMacBook-Pro.local>
2026-04-01 21:16:28 +0900

    Restore clear time rewards

--- lib/game/application/game_session_controller.dart
+++ lib/game/application/game_session_controller.dart
@@ -486,10 +486,10 @@ class GameSessionController extends StateNotifier<GameSessionState> {
 
   int _timeBonusForClearedTiles(int clearedTiles) {
     if (clearedTiles >= 4) {
-      return 3500;
+      return 3000;
     }
     if (clearedTiles >= 3) {
-      return 1750;
+      return 1500;
     }
     return 0;
   }
